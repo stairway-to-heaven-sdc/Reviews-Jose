f779d25d579e416ae5f151c3cff17b624fe9f0a5
NickRashad <nrsternes@gmail.com>
2019-05-11 16:08:03 -0500

    Seed database with multiple reviews

--- db/seed.js
+++ db/seed.js
@@ -1,12 +1,12 @@
-const { saveReview, retrieveAllReviews } = require('./db');
+const { saveReview } = require('./db');
 const faker = require('faker');
 const axios = require('axios');
 
 const seedDb = ( bizCount = 100 ) => {
-  let reviews = [];
   let hipsterReviews = [];
   let meatReviews;
   let starReviews = [];
+  // Get Hipser Reviews
   axios.get('http://hipsterjesus.com/api/')
   .then(({data}) => {
     // Split paragraph into variable lengths
@@ -20,42 +20,50 @@ const seedDb = ( bizCount = 100 ) => {
       hipsterReviews.push(curr);
       start = end;
     }
+    // Get Bacon Ipsum Reviews
     axios.get('https://baconipsum.com/api/?type=all-meat&paras=25&start-with-lorem=1')
     .then(({data}) => {
-      meatReviews = data
+      meatReviews = data;
+      // Get Reviews by Star Rating
       let args = [];
       for (let star = 1; star <= 5; star++) {
-        for(let i = 0; i <= 7; i++){
+        for(let i = 0; i <= 7; i++) {
           args.push( axios.post(`http://reviews.azurewebsites.net/?stars=${star}`) );
         }
       }
       axios.all(args)
       .then(axios.spread( (...allReviews) => {
-        for (let {data} of allReviews){
+        for (let {data} of allReviews) {
           starReviews.push(cleanData(data));
         }
-        // console.log(hipsterReviews.length, meatReviews.length, starReviews.length)
 
         const fullReviews = [ ...hipsterReviews, ...meatReviews, ...starReviews];
         const len = fullReviews.length;
+        const reviewBatch = [];
         const getReview = () => fullReviews[getRandom(0,0,len)];
         for (let i = 1; i <= bizCount; i++) {
           // For loop to generate variable length of reviews
-          // Push reviews into promise to be saved into DB at one time
-          // Retrieve all reviews upon finishing
-          // Create script in package.json
-          let review = {
-            uId: getRandom(1, 0, 99),
-            bId: i,
-            rating: getRandom(0, 1, 5),
-            reviewText: getReview(),
-            checkin: getRandom(0, 0, 2) ? false : true,
-            useful: generateUsers(),
-            funny: generateUsers(),
-            cool: generateUsers(),
-          };
-          console.log(review);
+          let randReviews = getRandom(0, 0, 20);
+          for (let j = 0; j < randReviews; j++) {
+            let date = faker.date.between('2013-01-01', '2019-5-30');
+            let review = {
+              uId: getRandom(1, 0, 99),
+              bId: i,
+              rating: getRandom(0, 1, 5),
+              reviewText: getReview(),
+              checkin: getRandom(0, 0, 2) ? false : true,
+              useful: generateUsers(),
+              funny: generateUsers(),
+              cool: generateUsers(),
+              createdAt: date,
+              updatedAt: date,
+            };
+            // Push reviews into promise to be saved into DB at one time
+            reviewBatch.push(review);
+          }
         }
+        saveReview(reviewBatch)
+        .then((stat) => console.log(`**Success**\n\n`, stat.slice(0,6), `Review Total: ${stat.length}\nFirst 7 Reviews Displayed`));
       }))
     })
   })
@@ -73,10 +81,9 @@ const cleanData = (data) => decodeURI(data.split(`-Star Review</h2>`)[1].split(`
 
 const generateUsers = () => {
   let users = [];
-  let num = getRandom(0,0,5);
+  let num = getRandom(0,0,3);
   for (let i = 0; i < num; i++){
-    users.push( { username: faker.name.findName(), uId: (1,0,99) } );
+    users.push( { username: faker.name.findName(), uId: getRandom(1,0,99) } );
   }
   return users;
 };
-
