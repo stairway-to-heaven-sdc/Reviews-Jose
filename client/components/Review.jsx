0a4395c83b2a58af3fa8ea8558abdabbbf88d217
NickRashad <nrsternes@gmail.com>
2019-05-30 22:09:38 -0500

    Display Ratings for Review

--- client/components/Review.jsx
+++ client/components/Review.jsx
@@ -2,10 +2,12 @@ import React from 'react';
 
 const Review = (props) => {
   let { review } = props;
+  let rating = `i-stars i-stars--${review.rating * 10}`;
   let date = (new Date(review.createdAt).toLocaleString()).split(',')[0];
   return (
   <div className="review-content">
-    <div className="rating-date"> Rating {review.rating} {date} </div>
+    <div className={rating}></div><span className="rating-date"> {date} </span>
+    {/* <div className="rating-date"> {date} </div> */}
     <p>{review.reviewText}</p>
   </div>
   // <div className="review-footer">
