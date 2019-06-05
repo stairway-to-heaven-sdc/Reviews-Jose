58d035cf7925add50cd260a34a74334d7cfd955b
NickRashad <nrsternes@gmail.com>
2019-06-05 01:11:59 -0500

    Display Trust Banner

--- client/components/Search.jsx
+++ client/components/Search.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import Compose from './Compose.jsx';
+import * as Icon from './images/Icons.jsx';
 
 const Search = (props) => (
   <div className="top-row">
@@ -8,8 +9,17 @@ const Search = (props) => (
       <div className="text">Yelp users haven't asked any questions yet about <b>{props.bizName}.</b></div>
       <div className="question">Ask a Question</div>
     </div>
-    <div className="Section2-Reccommended">
+    <div className="Section2-Recommended">
       <h2 className="header">Recommended Reviews <b>for {props.bizName}</b></h2>
+      <div className="trust-banner">
+        <div className="trust-arrange">
+          <div className="trust-logo"> <span className="icon"><Icon.yelpSvgLogo /></span> </div>
+          <div className="trust-text">
+            <span className="legal-text"><b>Your trust is our top concern,</b> so business can't pay to alter or remove their reviews. <a className="trust-highlight">Learn more.</a></span>
+          </div>
+        </div>
+      </div>
+
     </div>
   </div>
 );
