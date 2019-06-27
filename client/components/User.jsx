5ce5a13ac6c7061a4097013b52b4d5bfbbd1cc3d
NickRashad <nrsternes@gmail.com>
2019-06-26 22:28:48 -0500

    Optimize Users service

--- client/components/User.jsx
+++ client/components/User.jsx
@@ -9,7 +9,7 @@ const User = (props) => {
     axios.get(`/user/${props.user}`)
     .then(({data}) => setUser(data));
   },[]);
-  let {username, city, state, friends, photo, elite} = userInfo;
+  let {username, city, state, photo, elite, friendCount, reviewCount, photoCount } = userInfo;
 
 
   return (
@@ -21,9 +21,9 @@ const User = (props) => {
       <div className="user-story">
         <div className="username"> {username} </div>
         <div className="location"> {`${city}, ${state}`} </div>
-        <div ><span className="icon-user-review" style={{width: '18px', height: '18px'}}> <Icon.friends /> </span> <b>5</b> friends </div>
-        <div ><span className="icon-user-review" style={{width: '18px', height: '18px'}}> <Icon.reviewCount /> </span> <b>5</b> reviews </div>
-        <div ><span className="icon-user-review" style={{width: '18px', height: '18px'}}> <Icon.photos /> </span>  <b>5</b> photos </div>
+        <div><span className="icon-user-review" style={{width: '18px', height: '18px'}}> <Icon.friends /> </span> <b>{friendCount}</b> friends </div>
+        <div><span className="icon-user-review" style={{width: '18px', height: '18px'}}> <Icon.reviewCount /> </span> <b>{reviewCount}</b> reviews </div>
+        <div><span className="icon-user-review" style={{width: '18px', height: '18px'}}> <Icon.photos /> </span> <b>{photoCount}</b> photos </div>
         { elite &&
         <div className="elite"> <span>{elite}</span> </div>
         }
