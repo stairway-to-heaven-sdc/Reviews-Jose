d667ca6c3bff54beee955ef4f07b636abd847c7a
Jose Valadez <vinmajj@gmail.com>
2019-07-11 21:44:20 -0700

    Build CRUD Apis

--- server/server.js
+++ server/server.js
@@ -2,20 +2,100 @@ const express = require('express');
 const app = express();
 const bodyParser = require('body-parser');
 const port = process.env.PORT || 3002;
-const { retrieveByBiz, retrieveByUser, retrieve1Review } = require('../db/dbReviews');
-const { saveUsers, retrieveUsersById } = require('../db/dbUsers');
+const { retrieveByBiz, retrieveByUser, retrieve1Review, saveReview, Review} = require('../db/dbReviews');
+// const { saveUsers, retrieveUsersById } = require('../db/dbUsers');
+const morgan = require('morgan');
 
+// Middleware
 app.use(bodyParser.urlencoded({ extended: true }));
+app.use(morgan('dev'));
+// parse application/json
+app.use(bodyParser.json());
 app.use(express.static('public'));
 
+// Changes to the URL in UI should allow for new review's by bId pull
+app.use('/reviews/:bId', express.static('public'));
+
+
+/**
+ * CRUD APIs
+ * 
+ */
+// GET all reviews
 app.get('/reviews/business/:bId', (req, res) => {
+
   let { bId } = req.params;
+  console.log(bId, `this is the business ID`)
   retrieveByBiz(bId).then((reviews) =>{
     res.send({reviews});
   })
   .catch((err) => console.log(err));
 });
 
+// POST new review 
+app.post('/reviews/new/:bId', 
+  (req, res) => {
+    const review = req.body;
+    review.bId = Number(req.params.bId);
+    // add funny, cool, useful properties
+    review.funny = [];
+    review.cool = [];
+    review.useful = [];
+    // save review by Business ID (bId)
+    saveReview(review)
+    .then((result) => {
+      res.send(result);
+    });
+  });
+
+// PUT update one review record
+app.put('/review/modifyReview/', 
+  (req,res) => {
+  // review _id
+  const rvwId = req.query._id;
+  // body contains the revie text and rating
+  const body = req.body;
+  // determine if the review has been updated with the body
+  Review.findById(rvwId)
+    .then(review => {
+      // get current values
+      const currReviewText = review.reviewText;
+      const currRating = review.rating;
+      // compare current values with values from client, 
+      // if the values are the same, respond with the review
+      if (currRating === body.rating && currReviewText === body.reviewText) {
+        res.send(review);
+      } else {
+      // else, update the values with new values from client
+        review.reviewText = body.reviewText;
+        review.rating = body.rating;
+        review.save((err, review) => {
+          if (err) {
+            res.send(err);
+          }
+          res.send(review);
+        })
+      }
+    })
+
+    .catch(err => res.send(err));
+}); 
+
+// DELETE one review record
+app.delete('/review/remove', 
+  (req, res) => {
+    const rvwId = req.query._id;
+    Review.findByIdAndDelete(rvwId)
+      // returns the review that was deleted
+      .then(response => res.send(response))
+      // returns error
+      .catch(err => res.send(err));
+});
+
+/**
+ * END OF CRUD APIs
+ * 
+ */
 app.get('/reviews/user/:uId', (req, res) => {
   let { uId } = req.params;
   retrieveByUser(uId).then((reviews) =>{
@@ -28,7 +108,6 @@ app.get('/reviews/search/:bId', (req, res) => {
   let { bId } = req.params;
   retrieveByBiz(bId).then((reviews) =>{
     // Search review text within each review and return
-
     res.send({reviews});
   })
   .catch((err) => console.log(err));
